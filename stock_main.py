827e16cfbe9b288377e01a737da17b70ca38392f
BruceZhao <zrz0015@gmail.com>
2018-04-11 16:28:22 +0800

    draw line finished

--- stock_main.py
+++ stock_main.py
@@ -1,7 +1,33 @@
-import pandas
-#import pandas_datareader as pdr
-import datetime
+# -*- coding: utf-8 -*-
+import re
+import tushare as ts
+import matplotlib.pyplot as plt
+from matplotlib.pylab import datestr2num
 
-start = datetime.datetime(2018, 1, 1)
-end = datetime.date.today()
+code = '600606'
+#get stock name
+if re.match(r'\d{6}', code):
+    stock_list = ts.get_stock_basics()
+    if code in stock_list.index:
+        name = stock_list.ix[code]['name']
+        print(name)
+
+        # get history
+        res_table = ts.get_k_data(code, '2018-01-01', '2018-01-05')
+        print(res_table)
+        #string to date
+        print(res_table['date'])
+        #x_date = [datestr2num(i) for i in res_table['date']]
+        #draw
+        plt.figure(figsize=(10, 5))
+        #matplotlib doesn't support chinese
+        plt.title(u"close - date")
+        plt.xlabel(u"date")
+        plt.ylabel(u"price")
+        plt.plot_date(res_table['date'], res_table['close'], '-', label=u"stock at closing time")
+
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
 
