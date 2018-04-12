a9d0778545185eee40d7bec49326d74a90d7ca02
BruceZhao <zrz0015@gmail.com>
2018-04-12 12:35:27 +0800

    stock news finished

--- stock_news.py
+++ stock_news.py
@@ -1,3 +1,26 @@
 import tushare as ts
+from datetime import datetime
+from datetime import timedelta
 
-print(ts.get_notices('600606', '2018-02-03'))
+
+code = '600606'
+start = '2018-02-01'
+end = '2018-02-10'
+start_date = datetime.strptime(start, '%Y-%m-%d')
+end_date = datetime.strptime(end, '%Y-%m-%d')
+
+res = None
+while start_date <= end_date:
+    try:
+        temp = ts.get_notices(code, start_date.strftime("%Y-%m-%d"))
+    except:
+        start_date += timedelta(days=1)
+        continue
+    print(temp)
+    if res is None:
+        res = temp
+    else:
+        res = res.append(temp, ignore_index=True)
+    start_date += timedelta(days=1)
+
+print(res)
